a0551e76626b39184df32f23c592ab2356214cdb
Srijita <srijitabhattacharjee123@gmail.com>
2023-01-10 01:44:12 +0530

    new file:   .env 	new file:   components/Cart.jsx 	new file:   components/Footer.jsx 	new file:   components/FooterBanner.jsx 	new file:   components/Layout.jsx 	new file:   components/Navbar.jsx 	new file:   components/Product.jsx 	new file:   components/index.js 	new file:   lib/client.js 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/_app.js 	deleted:    pages/_document.js 	modified:   pages/index.js 	modified:   sanity-ecommerce/schemas/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,33 @@
 import React from 'react'
-import {HeroBanner} from '../components/HeroBanner';
+import {client} from '../lib/client';
+import { HeroBanner, Product, FooterBanner } from '../components';
 
-const Home = () => {
-  return (
-    <>
-    HeroBanner
-    <div className='products-heading'>
-      <h2>Best Selling Products</h2>
-      <p>Shoes of many categories</p>
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
     </div>
-    <div className='products-container'>
-      {['Product 1', 'Product 2'].map(
-        (product) => product)}
+
+    <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
     </div>
-    Footer
-    </>
-  )
+
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home;
